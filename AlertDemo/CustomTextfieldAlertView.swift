aad7b408ec04205cfa6367f2eb38388ab8e5b8f0
JD Patel <iamjdpatel@gmail.com>
2021-04-01 12:45:33 +0530

    code refactoring

--- AlertDemo/CustomTextfieldAlertView.swift
+++ AlertDemo/CustomTextfieldAlertView.swift
@@ -18,34 +18,23 @@ struct CustomTextfieldAlertView: View {
     @Binding var showCustomTextfieldAlert: Bool
     
     var body: some View {
-        
         ZStack {
-            
             Color(UIColor.black.withAlphaComponent(0.1))
-            
             ZStack {
-                
                 VisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialDark))
                     .frame(width: width, height: height)
                     .cornerRadius(15)
-                
                 VStack(spacing: 0) {
-                    
                     Spacer()
-                    
                     Text("Title")
                         .font(.system(size: 18))
                         .padding(.bottom, 8)
-                    
                     TextField("Enter your name", text: $name)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: width - 30, height: 45)
                         .padding(.bottom, 8)
-                    
                     Divider()
-                    
                     HStack {
-                        
                         Button(action: {
                             self.showCustomTextfieldAlert.toggle()
                         }) {
@@ -54,9 +43,7 @@ struct CustomTextfieldAlertView: View {
                                 .frame(width: width/2, height: 45)
                                 .foregroundColor(.red)
                         }
-                        
                         Divider()
-                        
                         Button(action: {
                             self.showCustomTextfieldAlert.toggle()
                         }) {
@@ -64,17 +51,11 @@ struct CustomTextfieldAlertView: View {
                                 .font(.system(size: 18))
                                 .frame(width: width/2, height: 45)
                         }
-                        
                     }.frame(width: width, height: 45)
-                    
                 }
-                
             }.frame(width: width, height: height)
-            
         }
-        
     }
-    
 }
 
 struct CustomTextfieldAlertView_Previews: PreviewProvider {
